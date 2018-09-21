7a584d13134976e1460f2efb0b7917f48fa0f540
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2018-09-20 21:16:49 -0400

    just a try

---
+++ components/Typography/Title.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+const Title = styled.h1`
+  font-family: 'Source Sans Pro', sans-serif;
+  color: gray;
+  display: flex;
+`
+
+export default Title
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import NavbarContainer from '../components/NavbarContainer'
+import Title from '../components/Typography/Title'
+
+const Index = () => (
+  <NavbarContainer>
+    <Title>Title</Title>
+  </NavbarContainer>
+)
+export default Index
