1e2a128f77eb758ac1c4fd81d9079cbd3f168433
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2018-09-23 18:52:13 -0400

    minute layut setup. With 5 layouts.

---
+++ layouts/context.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { DEFAULT_LAYOUT } from "../constants/defaults"
+
+const LayoutSharedContext = React.createContext()
+
+export class LayoutProvider extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      layoutId: DEFAULT_LAYOUT
+    }
+  }
+
+  selectLayout = id => {
+    this.setState({
+      layoutId: id
+    })
+  }
+
+  resetLayout = () => {
+    this.setState({
+      layoutId: DEFAULT_LAYOUT
+    })
+  }
+
+}
